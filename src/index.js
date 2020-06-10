f2b416fb5af5d3b5c1b7bbbddb1c81ea3a14770c
marut.prasad <Marut.Prasad@philips.com>
2020-06-10 22:20:42 +0530

    Adding basic app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/app"
 
-const myfirstelement = <h1>Hello React!</h1>
-
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
