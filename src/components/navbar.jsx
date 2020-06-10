efbe091142a3b456a7bbf7e81c5f656fd7692afd
marut.prasad <Marut.Prasad@philips.com>
2020-06-11 01:38:19 +0530

    Navbar name and icon set

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../styles/navbar.css";
+import mainIcon from "../icons/mainIcon.png";
 
 class NavBar extends Component {
-    render() { 
-        return ( 
-            <nav className="navbar navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">NavBar</a>
-            </nav>
-         );
-    }
+  render() {
+    return (
+      <div className="background">
+        <img src={mainIcon} className="icon"></img>
+        <h2 className="header">Path Finder</h2>
+      </div>
+    );
+  }
 }
- 
+
 export default NavBar;
