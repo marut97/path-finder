d38c9b3edf9a20b792fcfd4d333e18fa9fedd92c
marut.prasad <Marut.Prasad@philips.com>
2020-06-10 22:34:13 +0530

    Adding navbar, icon and bootstrap package

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from './navbar';
 
 class App extends Component {
     render() { 
         return ( 
             <React.Fragment>
+                <NavBar/>
             </React.Fragment>
          );
     }
